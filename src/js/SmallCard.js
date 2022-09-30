e6081d2f4db5263dd286a4def41015f397e16a6f
KubaGithub92 <jakub.rosol@email.cz>
2022-09-30 12:13:59 +0200

    created Class for the small cards

---
+++ src/js/SmallCard.js
@@ -0,0 +1,44 @@
+export default class SmallCard {
+  constructor() {
+    this.getData();
+    this.element = document.createElement("div");
+  }
+
+  getData = async () => {
+    try {
+      let res = await fetch(
+        "https://test-api.codingbootcamp.cz/api/1485f022/events"
+      );
+      const data = await res.json();
+      console.log(data);
+      this.data = data;
+
+      this.renderEvent();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  renderEvent = () => {
+    for (let i = 1; i < this.data.length; i++) {
+      this.element.classList.add("cards");
+      this.element.innerHTML += `
+        <div class="card">
+          <h3>${this.data[i].name}</h3>
+          <button>More</button>
+        </div>
+        `;
+      document.body.appendChild(this.element);
+    }
+    // this.data.forEach((event, i) => {
+    //   this.element.classList.add("cards");
+    //   this.element.innerHTML += `
+    //   <div class="card">
+    //     <h3>${event.name}</h3>
+    //     <button>More</button>
+    //   </div>
+    //   `;
+    //   document.body.appendChild(this.element);
+    // });
+  };
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 import MainCard from "./MainCard";
+import SmallCard from "./SmallCard";
 
 // Creating feature cards
-const FeatureEvent = new MainCard();
+const featureEvent = new MainCard();
+
+// Creating small event card
+const smallEvent = new SmallCard();
